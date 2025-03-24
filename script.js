fab26cf1f3497e240dba683459f53e4206ab9b11
Ankit Kumar <annuank127@gmail.com>
2025-03-24 10:03:34 +0530

    final commit

--- script.js
+++ script.js
@@ -18,3 +18,27 @@ window.addEventListener("click", (event) => {
     dropdown.style.display = "none";
   }
 });
+
+
+fetch("https://dummyjson.com/products")
+.then((res)=>res.json())
+.then((data)=>{console.log(data);
+   theData(data.products)})
+.catch((err)=>console.error(err))
+
+function theData(arr){
+  let container = document.querySelector("#container")
+
+  arr.forEach((prod) => {
+    const {brand,price,rating,thumbnail,title}=prod;
+    const proCard = document.createElement("div")
+    proCard.innerHTML= `<img src='${thumbnail}' alt=''/>
+    <h3>${title} - ${brand}</h3>
+    <h6>${price}</h6>
+    <h5>${rating}</h5>
+    <button>BookNow</button>
+    `
+    container.appendChild(proCard)
+  })
+  
+}
